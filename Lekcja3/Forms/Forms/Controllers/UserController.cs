9d90ed935d0c735187cba8db4c67d49be318e1d3
Marek Zajac <contact@zajacmarek.com>
2020-04-08 21:37:59 +0200

    Lesson 3

---
+++ Lekcja3/Forms/Forms/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Forms.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forms.Controllers
+{
+    public class UserController : Controller
+    {
+        private IWebHostEnvironment _hostingEnvironment;
+
+        public UserController(IWebHostEnvironment webHostEnvironment)
+        {
+            _hostingEnvironment = webHostEnvironment;
+        }
+
+        [Route("", Name = "Test")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            var vm = new EditUserViewModel
+            {
+                FirstName = "Marek",
+                LastName = "Zając",
+                Age = 28,
+                Description = "DESCRIPTION",
+                Id = 11
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditUserViewModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(data);
+            }
+
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+
+            if (!Directory.Exists(uploads))
+            {
+                Directory.CreateDirectory(uploads);
+            }
+
+            var filePath = Path.Combine(uploads, data.MyFile.FileName);
+
+            data.MyFile.CopyTo(new FileStream(filePath, FileMode.Create));
+
+            return RedirectToRoute("Test");
+        }
+    }
+}
---
+++ Lekcja3/Forms/Forms/Models/EditUserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms.Models
+{
+    public class EditUserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Imię jest wymagane")]
+        [Display(Name ="Imię")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage ="Hasło musi mieć co najmniej 5 znaków")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Text)]
+        public int Age { get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        public IFormFile MyFile { get; set; }
+    }
+}
